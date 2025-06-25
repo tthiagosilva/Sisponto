02dad8d40904c1acf48786826257cf7d6bdbec46
tthiagosilva <160064804+tthiagosilva@users.noreply.github.com>
2025-06-25 20:37:46 -0300

    Update configuracoes.js

--- configuracoes.js
+++ configuracoes.js
@@ -267,6 +267,7 @@ class Configuracoes {
                       </div>
                     </div>
                   </div>
+                  `
                 }
               }
                     
